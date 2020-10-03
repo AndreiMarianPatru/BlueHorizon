3237993308c3944503a0da7a60acd9c054b691e4
Harrison Went <43639471+HarrisonWent@users.noreply.github.com>
2020-10-03 16:05:52 +0100

    Session Timer
    Added a timer which tracks the whole session length, exports when application is quit. Formatted in hrs,mins,secs,ms.
    
    Might want to check the order in which application quit method is called as it is possible the one for analytics export may be executed before the session timer sends it to the anlytics.

---
+++ Blue Horizon/Assets/scripts/SessionTimer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SessionTimer : MonoBehaviour
+{
+    private static SessionTimer SessionTimerinstance;
+
+    //Counter of whole session time
+    private Double Timer = 0.0f;
+
+    //Ensure only one session timer exists
+    private void Awake()
+    {
+        //Destroy if already exists
+        if (SessionTimerinstance && SessionTimerinstance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            SessionTimerinstance = this;
+        }
+
+        //Keep alive over multiple scenes
+        DontDestroyOnLoad(gameObject);
+    }
+
+    //Increment timer
+    private void Update()
+    {
+        Timer += Time.deltaTime;
+        Debug.Log("Session length: " + ConvertSecondsToTime(Timer));
+    }
+
+    //Export the session length to the analytics
+    private void OnApplicationQuit()
+    {
+        AnalyticsExport.SendToExport("Session length: " + ConvertSecondsToTime(Timer));
+    }
+
+    //Convert seconds to: hours, minutes, seconds, miliseconds
+    public static string ConvertSecondsToTime(Double Seconds)
+    {
+        return TimeSpan.FromSeconds(Seconds).ToString(@"hh\:mm\:ss\:fff");
+    }
+}
