12237177cb07bffe6e8f8f8594cee93e1e2832b9
Andrei Marian Patru <andreiuttzzumarian00@gmail.com>
2020-12-03 13:49:11 +0000

    reposition all biomes and sandwalls

---
+++ Blue Horizon/Assets/getsize.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class getsize : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 size = GetComponent<Renderer>().bounds.size;
+        Debug.Log("size"+size);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
